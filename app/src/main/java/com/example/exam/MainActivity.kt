5e83d0ca68c9025200094cbdb3a60eb85b229ae0
gizuDev <birknergard@gmail.com>
2024-10-24 12:45:51 +0200

    Added navController and serialization dependencies.

--- app/src/main/java/com/example/exam/MainActivity.kt
+++ app/src/main/java/com/example/exam/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.example.exam.ui.theme.ExamTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +21,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ExamTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                val nav = rememberNavController()
+                NavHost(startDestination = "", navController = nav){
+
                 }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExamTheme {
-        Greeting("Android")
     }
 }
