c01b36f78967349d9a2e0b78e3145f9543bfc69d
gizuDev <birknergard@gmail.com>
2024-10-31 13:31:43 +0100

    removed import statement, pull on macbook.

--- app/src/main/java/com/example/exam/Repository.kt
+++ app/src/main/java/com/example/exam/Repository.kt
@@ -1,14 +1,11 @@
 package com.example.exam
 
-import android.util.Log
-import com.example.exam.dataClasses.ApiData
 import com.example.exam.dataClasses.Character
 
 object Repository {
 
     suspend fun loadCharactersFromApi(page : Int) : List<Character>{
         val response = RetrofitInstance().getAllCharactersFromApi(page)
-        //response.result.forEach { char -> println(char.name) }
         return response.result
     }
 
